f4f4bd981ed4b639b741ba36fb4a8601d50b367e
AY_28 <77998267+eunsu28@users.noreply.github.com>
2023-01-02 11:48:33 +0000

    2023/01/02 happy new year
    and you did well done through the 2022

--- main.swift
+++ main.swift
@@ -1,7 +1,11 @@
 print("Hello, World")
 //start
 
+//pg.5
 let name = "AYDEN" //let == const in js
 var height = 179 //var == let in js
 let test1 = name + String(height)
+let test2 = "\(name)'s height is \(height)"
 print(test1)
+print(test2)
+//

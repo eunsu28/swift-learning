d00713439c2ba8efcae64ee46f43a1b220a9ba58
AY_28 <77998267+eunsu28@users.noreply.github.com>
2023-01-03 08:20:15 +0000

    list, if, for 2023/01/03

--- main.swift
+++ main.swift
@@ -9,3 +9,24 @@ let test2 = "\(name)'s height is \(height)"
 print(test1)
 print(test2)
 //
+
+print("\n//////////////////////////////////////\n") //\n to enter
+
+//pg.6 ~ 7
+let list = ["f4u", "a10", "a1", "f15", "f18"]
+print(list)
+
+let score = 91
+if score > 90 {
+  print("you are A")
+}
+
+let score_list = [100, 90, 99, 80, 83, 87, 97]
+for s in score_list {
+  if s >= 90 {
+    print("\(s): A")
+  } else {
+    print("\(s): B")
+  }
+}
+//
